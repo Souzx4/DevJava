ab9fb40fe8dd7eca3c9d30d96f9ca1b672729872
Gilmar Sousa De Brito <gilmarsousa717@gmail.com>
2025-12-23 17:00:08 -0300

    up

---
+++ ProgramaMedia/src/main/java/com/mycompany/programamedia/ProgramaMedia.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.programamedia;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Eletel_Monitor
+ */
+public class ProgramaMedia {
+
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Primeira nota: ");
+        float n1 = teclado.nextFloat();
+        System.out.println("Segunda nota: ");
+        float n2 = teclado.nextFloat();
+        float m = (n1+n2)/2;
+        System.out.println("Sua media é " + m);
+        if (m>9){
+            System.out.println("Parabens");
+        }
+    }
+}
