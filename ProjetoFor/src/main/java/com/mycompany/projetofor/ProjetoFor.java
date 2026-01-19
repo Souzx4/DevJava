601bedd24fdedac8f52258dac7f81e38993397bd
Gilmar Sousa De Brito <gilmarsousa717@gmail.com>
2026-01-19 16:39:20 -0300

    up

---
+++ ProjetoFor/src/main/java/com/mycompany/projetofor/ProjetoFor.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.projetofor;
+
+/**
+ *
+ * @author Eletel_Monitor
+ */
+public class ProjetoFor {
+
+    public static void main(String[] args) {
+        for (int cc=1; cc < 100; cc+=10){
+            System.out.println(cc);
+            for (int dd=0; dd <1; dd++){
+                System.out.println("mais 10");
+            }
+        }
+    }
+}
