d7f72bd0c358600743460424aad3577d3f6fd3ea
Gilmar Sousa De Brito <gilmarsousa717@gmail.com>
2026-01-22 11:52:40 -0300

    up

---
+++ Funcao4/src/main/java/com/mycompany/funcao4/Funcao4.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.funcao4;
+
+/**
+ *
+ * @author Eletel_Monitor
+ */
+public class Funcao4 {
+
+    public static void main(String[] args) {
+        Fatorial f = new Fatorial();
+        f.setValor(5);
+        System.out.print(f.getFormula());
+        System.out.println(f.getFatorial());
+    }
+}
